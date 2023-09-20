641680657a103d39138d040db44aa6798d25afbf
acremers22 <acremers@umich.edu>
2023-09-20 09:36:49 -0400

    second commit

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -7,9 +7,9 @@ class Rectangle():
 
     # YOUR CODE HERE
 
-        def __init__(self, width, height):
-            self.width = width
-            self.height = height
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
     # Create the "__str__" method
     #
@@ -18,7 +18,9 @@ class Rectangle():
 
     # YOUR CODE HERE
 
-
+    def __str__(self):
+        return "A rectangle with width " + str(self.width) + " and height " + str(self.height)
+        
 
     # Create the "verify_input" method
     #
@@ -28,7 +30,11 @@ class Rectangle():
 
     # YOUR CODE HERE
 
-
+    def verify_input(self):
+        if self.width > 0 and self.height > 0:
+            return True
+        else:
+            return False
 
     # Create the "area" method
     #
@@ -37,6 +43,11 @@ class Rectangle():
 
     # YOUR CODE HERE
 
+    def area(self):
+        if self.verify_input() is False:
+            return "Invalid input"
+        else:
+            return self.width * self.height
 
 
     # Create the "perimeter" method
@@ -45,6 +56,12 @@ class Rectangle():
     # Otherwise, it returns the perimeter of the rectangle.
 
     # YOUR CODE HERE
+
+    def perimeter(self):
+        if self.verify_input() is False:
+            return "Invalid input"
+        else:
+            return (self.width + self.height) * 2
     
 
 
